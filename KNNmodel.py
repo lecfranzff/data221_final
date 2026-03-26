02bd69b72b182514b5485a07a99bdb212e6546aa
nikitasurikov123-code <nikitasurikov123@gmail.com>
2026-03-26 12:57:34 -0600

    cleaned the dataset

--- KNNmodel.py
+++ KNNmodel.py
@@ -25,6 +25,28 @@ for cc in data_frame["Engine"]:
         cleaned_engine.append(cc)
     else:
         cleaned_engine.append(cc)
-
+#new engine data
 data_frame["Engine"] = cleaned_engine
+#fill in missing values with the median
+median_for_engine = data_frame["Engine"].median()
+data_frame["Engine"] = data_frame["Engine"].fillna(median_for_engine)
+
+#some values in the drivetrain are missing, this will remove them
+data_frame = data_frame.dropna(subset = ["Drivetrain"])
+
+#fill in median for missing values of seating capacity
+median_for_seats = data_frame["Seating Capacity"].median()
+data_frame["Seating Capacity"] = data_frame["Seating Capacity"].fillna(median_for_seats)
+
+#fill in median for missing values of fuel tank capacity
+median_for_fuel = data_frame["Fuel Tank Capacity"].median()
+data_frame["Fuel Tank Capacity"] = data_frame["Fuel Tank Capacity"].fillna(median_for_fuel)
+
+#if we want to see similar cars, keep a copy of the cleaned dataset for later
+original_data = data_frame.copy()
+
+#converts text into values
+data_frame = pandas.get_dummies(data_frame, drop_first = True)
+
+
 
